5c3e376a9ef76b373640e6a7a74481cba2d9c212
Ray-Xavier-2021 <rayxavier.frontenddev.2021@gmail.com>
2023-04-24 20:28:43 -0400

    Implemented url routing to project view

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,13 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
 
+from projects.views import ProjectView
+from rest_framework import routers
+
+route = routers.DefaultRouter()
+route.register('', ProjectView, basename='projects')
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects', include(route.urls))
 ]
 
 # Media Static Files Configurations
